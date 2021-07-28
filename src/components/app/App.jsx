45b941276747c84f04cddf32763b9881497c4ef9
Katherine Tam <katherinemtam@gmail.com>
2021-07-27 23:29:45 -0700

    add routes to App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AllHoroscopes from '../../containers/AllHoroscopes';
+import DetailPage from '../../containers/DetailPage';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 
-export default function App() {
-  return <AllHoroscopes/>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <main>
+            <Switch>
+              <Route path="/" exact={true}
+                render={routerProps => (
+                  <AllHoroscopes {...routerProps} />
+                )}
+              />
+
+              <Route path="/:sign" exact={true}
+                render={routerProps => (
+                  <DetailPage {...routerProps} />
+                )}
+              />
+
+              <Redirect to="/" />
+
+            </Switch>
+          </main>
+        </Router>
+      </div>
+    );
+  }
 }
+
+export default App;
