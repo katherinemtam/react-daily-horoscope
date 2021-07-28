dc6da3d022012f86d1881bee119c2956a157a6dd
Katherine Tam <katherinemtam@gmail.com>
2021-07-27 20:55:17 -0700

    connect App.jsx to AllHoroscopes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import AllHoroscopes from '../../containers/AllHoroscopes';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <AllHoroscopes/>;
 }
