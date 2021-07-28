f333e9fed465f4f3eee2bddc61c174532ae02aee
Katherine Tam <katherinemtam@gmail.com>
2021-07-27 23:13:06 -0700

    separate fetchHoroscopes functions

--- src/services/horoscopes.js
+++ src/services/horoscopes.js
@@ -1,15 +1,15 @@
+export const getHoroscope = async sign => {
+  const res = await fetch(`https://aztro.sameerkumar.website/?sign=${sign}&day=today`, { method: 'POST' });
+  return res.json();
+};
+
 export const fetchHoroscopes = async () => {
 
   const signs = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
 
-  const getFetch = async sign => { 
-    const res = await fetch(`https://aztro.sameerkumar.website/?sign=${sign}&day=today`, { method: 'POST' });
-    return res.json();
-  };
-
   const horoscope = await Promise.all(
     signs.map(async sign => {
-      const fetchedInfo = await getFetch(sign);
+      const fetchedInfo = await getHoroscope(sign);
       return { sign, ...fetchedInfo };
     })
   );
